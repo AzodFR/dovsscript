8f67e28dbf3094890db6d1c9d02908d48c69d2e9
Azod <azod.contact@gmail.com>
2022-07-13 21:26:53 +0200

    add rpc

--- components/Login.vue
+++ components/Login.vue
@@ -22,22 +22,19 @@ export default {
     return {
       wait: false,
       options: [
-        // "https://dovx-wax-rpc.global.binfra.one",
-        "https://api.wax.greeneosio.com",
         "https://wax.cryptolions.io",
-        // "https://api-wax.eosauthority.com",
-        "https://wax.greymass.com",
-        // "https://chain.wax.io/",
-        // "https://wax.pink.gg/",
-        "https://api.waxsweden.org",
-        "https://wax.eosusa.news",
-        "https://wax.eosphere.io",
+        "https://query.3dkrender.com",
+        "https://api.wax.alohaeos.com",
         "https://wax.eu.eosamsterdam.net",
-        /*"https://wax.alohaeos.com",
-        "https://wax.waxsweden.com",
-        "https://wax.pink.com",
-        "https://wax.dapplica.com",
-        "https://wax.eosphere.com",*/
+        "https://wax.blokcrafters.io",
+        "https://api-wax-mainnet.wecan.dev",
+        "https://wax.cryptolions.io",
+        "https://wax.dapplica.io",
+        "https://wax.eosdac.io",
+        "https://wax.eoseoul.io",
+        "https://api.wax.liquidstudios.io",
+        "https://wax.api.eosnation.io",
+        "https://api.waxsweden.org",
       ],
       selected: "https://wax.cryptolions.io",
     };
@@ -61,7 +58,7 @@ export default {
   mounted() {
     if (localStorage.getItem("rpc")) {
       if (localStorage.getItem("rpc") == "random" &&  (!localStorage.getItem("blockedRPC") || localStorage.getItem("blockedRPC") == "false")) {
-        const rn = parseInt(Math.random() * (7 - 0) + 0)
+        const rn = parseInt(Math.random() * this.options.length)
         this.selected = this.options[rn]
       }
       else {
